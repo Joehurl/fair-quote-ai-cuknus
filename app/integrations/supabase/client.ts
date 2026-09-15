7e03d06daccd9962566ab7da21741900b07c015a
newly.dev <api@newly.dev>
2026-09-15 23:47:47 +0000

    build: Prepare for initial iOS build

---
+++ app/integrations/supabase/client.ts
@@ -0,0 +1,18 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import type { Database } from './types';
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = "https://yupmxllzjaycbpzfcmsn.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1cG14bGx6amF5Y2JwemZjbXNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0ODYyOTYsImV4cCI6MjEwNTA2MjI5Nn0.tFIUjGPMX1igLVZt4zQF7fGG7jDznXb92Mc6Xf0AZ9o";
+
+// Import the supabase client like this:
+// import { supabase } from "@/integrations/supabase/client";
+
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
